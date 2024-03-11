5f37bdf78952598fa19cd784421dd04c3e793bff
Hagar Serag <114977779+Eng-hager2000@users.noreply.github.com>
2024-03-11 17:49:42 +0200

    Add files via upload

---
+++ hms_app/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "HMS App",
+    'author': "Eng. Hagar Serag",
+    'category': 'healthy',
+    'version': '17.0.0.1.0',
+    'depends': ['base',
+                ],
+    'application': True,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/base_menu.xml',
+        'views/patient.xml',
+        'views/department.xml',
+        'views/doctor.xml',
+    ],
+}
